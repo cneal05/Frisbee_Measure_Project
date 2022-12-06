a3ba97fe4aea2ead658ce11d02657cb80b87bf63
Cneal05 <cneal05@charlottesvilleschools.org>
2022-12-06 13:44:16 -0500

    daadadafasfd

--- code_for_project/code.py
+++ code_for_project/code.py
@@ -1,1 +1,14 @@
-print("Hello World")
+# type: ignore
+import adafruit_mpu6050
+import busio
+import board
+import time
+#assigns the scl to GP6 and assigns sda to GP7 on the pico board
+sda_pin = board.GP6
+scl_pin = board.GP7
+i2c = busio.I2C(scl_pin, sda_pin)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+while True:
+    print(mpu.gyro)
+    time.sleep(1)
