f980dbac0a27af757dff33104a3bb43b04d6751d
Cneal05 <cneal05@charlottesvilleschools.org>
2023-02-03 13:46:49 -0500

    new iteration of code

--- code_for_project/First_Iteration_of_Code.py
+++ code_for_project/First_Iteration_of_Code.py
@@ -9,23 +9,33 @@ sda_pin = board.GP6
 scl_pin = board.GP7
 buttonPin = digitalio.DigitalInOut(board.GP17)
 buttonPin.direction = digitalio.Direction.INPUT
-buttonPin.pull = digitalio.Pull.DOWN 
+buttonPin.pull = digitalio.Pull.UP 
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
+mpu.gyro_range = 3
 counter = 0
 list_x = []
 list_y = []
 list_z = []
-timer = time.monotonic()
-while buttonPin.value == True:
+list_time = []
+while buttonPin.value == False:
     pass
-    print("PASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
-# button syntax still needed.
+    #print("Pass")
+timer = time.monotonic()
 while True:
     x_angular_velocity = mpu.gyro[0]
     y_angular_velocity = mpu.gyro[1]
     z_angular_velocity = mpu.gyro[2]
     list_x = [list_x, x_angular_velocity]
     list_y = [list_y, y_angular_velocity]
-    list_z = [list_z, z_angular_velocity]
-    print(z_angular_velocity)
+    list_z.append(z_angular_velocity)
+    list_time.append(time.monotonic())
+    #print(z_angular_velocity)
+    current_time = time.monotonic() - timer
+    if current_time > 2 and mpu.gyro[0]+mpu.gyro[1]+mpu.gyro[2]<1:
+        break
+#break out of while true and save data
+Values=open(f"/data/{time.monotonic()}.csv","w")
+for i in range(len(list_z)):
+    Values.write(f"{list_time[i]}{list_z[i]}\n")
+Values.close
