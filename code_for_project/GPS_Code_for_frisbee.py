64da31a0887bce65211d4c7113019d5e196cd4d8
Cneal05 <cneal05@charlottesvilleschools.org>
2023-02-14 14:22:28 -0500

    gps code update

--- code_for_project/GPS_Code_for_frisbee.py
+++ code_for_project/GPS_Code_for_frisbee.py
@@ -3,7 +3,6 @@ import busio
 import board
 import time
 import digitalio
-import math
 
 import adafruit_gps
 
@@ -14,13 +13,15 @@ buttonPin = digitalio.DigitalInOut(board.GP17)
 buttonPin.direction = digitalio.Direction.INPUT
 buttonPin.pull = digitalio.Pull.UP 
 counter = 0
-list_x = []
-list_y = []
-list_z = []
+list_a = []
+list_s = []
 list_time = []
 
 uart = busio.UART(tx=TX_pin, rx=RX_pin, baudrate=9600, timeout=10)
 gps = adafruit_gps.GPS(uart, debug=False)
+
+gps.update()
+base_altitude = gps.altitude_m
 
 while buttonPin.value == False:
     pass
@@ -28,6 +29,7 @@ while buttonPin.value == False:
 timer = time.monotonic()
 last_print = time.monotonic()
 while True:
+    print(base_altitude)
     # Make sure to call gps.update() every loop iteration and at least twice
     # as fast as data comes from the GPS unit (usually every second).
     # This returns a bool that's true if it parsed new data (you can ignore it
@@ -35,55 +37,31 @@ while True:
     gps.update()
     # Every second print out current location details if there's a fix.
     current = time.monotonic()
-    if current - last_print >= 1.0:
-        last_print = current
-        if not gps.has_fix:
-            # Try again if we don't have a fix yet.
-            print("Waiting for fix...")
-            continue
-        # We have a fix! (gps.has_fix is true)
-        # Print out details about the fix like location, date, etc.
-        print("=" * 40)  # Print a separator line.
-        print(
-            "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
-                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
-                gps.timestamp_utc.tm_mday,  # struct_time object that holds
-                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-                gps.timestamp_utc.tm_min,  # month!
-                gps.timestamp_utc.tm_sec,
-            )
-        )
-        #print("Fix quality: {}".format(gps.fix_quality))
-        # Some attributes beyond latitude, longitude and timestamp are optional
-        # and might not be present.  Check if they're None before trying to use!
-        #if gps.satellites is not None:
-            #print("# satellites: {}".format(gps.satellites))
-        if gps.altitude_m is not None:
-            print("Altitude: {} meters".format(gps.altitude_m))
-        if gps.speed_knots is not None:
-            print("Speed: {} knots".format(gps.speed_knots))
+    last_print = current
+    if not gps.has_fix:
+        # Try again if we don't have a fix yet.
+        print("Waiting for fix...")
+        continue
+    if gps.altitude_m is not None:
+        list_a.append(gps.altitude_m - base.altitude)
+    if gps.speed_knots is not None:
+        list_s.append(gps.speed_knots)
+    # The two below lines print fix quality, and amount of satellites, not required but can be useful
+    # print("Fix quality: {}".format(gps.fix_quality))
+    # if gps.satellites is not None:
+    #   print("# satellites: {}".format(gps.satellites))
+    current_time = time.monotonic() - timer
+    if current_time > 2 and gps.speed_knots <1:
+        break
+    #break out of while true and save data
 
-    ''' x_angular_velocity = mpu.gyro[0]
-        y_angular_velocity = mpu.gyro[1]
-        z_angular_velocity = mpu.gyro[2]
-        list_x = [list_x, x_angular_velocity]
-        list_y = [list_y, y_angular_velocity]
-        list_z.append(z_angular_velocity)
-        list_time.append(time.monotonic())
-        #print(z_angular_velocity)
-        current_time = time.monotonic() - timer
-        if current_time > 2 and math.fabs(mpu.gyro[0]+mpu.gyro[1]+mpu.gyro[2])<1:
-            break
-        #break out of while true and save data
-        '''
-    Values=open(f"/data-{}-{}-{} {:02}:{:02}:{:02}".format(
-                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
-                gps.timestamp_utc.tm_mday,  # struct_time object that holds
-                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-                gps.timestamp_utc.tm_min,  # month!
-                gps.timestamp_utc.tm_sec,.csv),"w")
-    for i in range(len(list_z)):
-        Values.write(f"{list_time[i]}{list_z[i]}\n")
-    Values.close
+    # Grab parts of the time from the
+    # struct_time object that holds
+    # the fix time.  Note you might
+    # not get all data like year, day,
+    # month!        
+Values=open(f"/data/{gps.timestamp_utc.tm_mon}-{gps.timestamp_utc.tm_mday}-{gps.timestamp_utc.tm_year}/{gps.timestamp_utc.tm_hour,}.csv","w")
+#Values=open(f"/data/{gps.timestamp_utc.tm_mon}-{gps.timestamp_utc.tm_mday}-{gps.timestamp_utc.tm_year} {gps.timestamp_utc.tm_hour,}:{gps.timestamp_utc.tm_min,}:{gps.timestamp_utc.tm_sec}.csv","w")
+for i in range(len(list_z)):
+    Values.write(f"{list_time[i]}{list_z[i]}\n")
+Values.close
